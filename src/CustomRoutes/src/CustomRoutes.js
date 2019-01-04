e6c0b9ffe317a171f4bd98d1f77788f1196212eb
Getriax <nikodem.strawa@gmail.com>
2019-01-04 09:13:28 +0100

    routes

---
+++ src/CustomRoutes/src/CustomRoutes.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-underscore-dangle */
+const Rollbar = require('rollbar');
+
+let instance;
+
+class CustomRoutes {
+  constructor(robot) {
+    this.robot = robot;
+    this.routes = [];
+
+    this.setupMiddleware();
+  }
+
+  setupMiddleware() {
+    this.robot.server._events.request.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+      req.token = req.header('authorization');
+
+      next();
+    });
+  }
+
+  createRoute(path, callback, requireToken = true) {
+    if (this.routes.includes(path)) {
+      throw new Error(`Path: ${path} already existd`);
+    }
+
+    this.routes.push(path);
+    this.robot.router.post(path, async (req, res) => {
+      if (requireToken && req.token !== process.env.ROUTE_TOKEN) {
+        return res.status(401).send('Unauthorized');
+      }
+
+      try {
+        return callback(req, res);
+      } catch (error) {
+        Rollbar.error(error);
+        return res.status(500).send('Internal Error');
+      }
+    });
+  }
+}
+
+module.exports = {
+  getInstance: (robot) => {
+    if (!instance) {
+      instance = new CustomRoutes(robot);
+    }
+
+    return instance;
+  },
+  destroy: () => {
+    instance = null;
+  },
+};
