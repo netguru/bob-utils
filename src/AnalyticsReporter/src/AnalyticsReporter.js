73c899e207c00bb7b64b86cd83ca4607506733c8
Patryk Sadok <patryk.sadok@gmail.com>
2019-08-09 09:09:08 +0200

    Added label parameter

--- src/AnalyticsReporter/src/AnalyticsReporter.js
+++ src/AnalyticsReporter/src/AnalyticsReporter.js
@@ -8,17 +8,30 @@ class AnalyticsReporter {
     this.client = client;
   }
 
-  translate = ({ action: ea, category: ec, trackingId: tid, client: cid }) => ({
+  translate = ({
+    action: ea,
+    category: ec,
+    trackingId: tid,
+    client: cid,
+    label: el
+  }) => ({
     v: 1,
     t: "event",
     ea,
     ec,
+    el,
     tid,
     cid
   });
 
-  createEventUrl = (category, client, action) => {
-    const query = { action, category, trackingId: this.trackingId, client };
+  createEventUrl = (category, client, action, label) => {
+    const query = {
+      action,
+      category,
+      trackingId: this.trackingId,
+      client,
+      label
+    };
 
     const queryParams = Object.entries(this.translate(query)).map(
       ([key, value]) => {
@@ -29,9 +42,9 @@ class AnalyticsReporter {
     return queryParams.join("&").slice(0, -1);
   };
 
-  createEvent = category => async action => {
+  createEvent = category => async (action, label) => {
     try {
-      const url = this.createEventUrl(category, this.client, action);
+      const url = this.createEventUrl(category, this.client, action, label);
 
       await axios.post(this.analyticsUrl + url);
     } catch (error) {
